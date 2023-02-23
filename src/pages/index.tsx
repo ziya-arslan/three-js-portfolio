79ab263d28dd0fe5a9d5d4b90887113186f2a9bc
Ziya Arslan <102023322+ziya-madde22@users.noreply.github.com>
2023-02-23 23:18:09 +0600

    Added mac with animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,19 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main style={{ position: "relative" }}>
-        <div
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            zIndex: 10,
-            background: "rgba( 255, 255, 255, 0.25 )",
-            boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-            backdropFilter: "blur( 20px )",
-            borderRadius: "10px",
-            border: "1px solid rgba( 255, 255, 255, 0.18 )",
-          }}
-        ></div>
+        <div className="page"></div>
         <Model />
       </main>
     </>
